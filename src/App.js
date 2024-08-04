203fa449e1d1f2b5c8fb956851096c0dd80ff1b8
Ihor Stepanov <igorka2005@gmail.com>
2024-08-04 23:20:10 +0300

    on pc only available

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
 import { AddTask } from "./components/AddTask";
 import { ShowTask } from "./components/ShowTask";
+import { NoMobile } from "./components/NoMobile";
 import './App.css';
  
  
@@ -15,7 +16,21 @@ function App() {
   useEffect(() => {
     localStorage.setItem("tasklist", JSON.stringify(tasklist))
   }, [tasklist]);
- 
+  
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    
+    if (/android/i.test(userAgent) || /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      setIsMobile(true);
+    }
+  }, []);
+
+  if (isMobile) {
+    return <NoMobile />;
+  }
+
   return (
     <div className="App">
       <Header />
---
+++ src/components/NoMobile.js
@@ -0,0 +1,8 @@
+export const NoMobile = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>Обмежений доступ до сторінки через мобільний пристрій</h1>
+      <p>Відвідати сторінку можливо тіки с пк</p>
+    </div>
+  )
+}
